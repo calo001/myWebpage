b4b0234c9aff2cf0cbc404f51452db0aa9a17918
Carlos Lopez <calo_lrc@hotmail.com>
2019-03-31 21:46:41 -0600

    Basic and complete page

--- assets/javascript/entry.js
+++ assets/javascript/entry.js
@@ -1,1 +1,13 @@
 import '../css/app.scss'
+
+let menuActive = document.querySelector("#my_nav");
+document.querySelector(".floatingmenu").onclick = function(){
+  menuActive.classList.toggle("menu_active");
+};
+
+let elementsMenu = document.querySelectorAll(".menu_active a");
+elementsMenu.forEach( (el)=> {
+  el.onclick = function(){
+    menuActive.classList.remove("menu_active");
+  };
+} );
