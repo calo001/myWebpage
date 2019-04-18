6b30835bda65c39e27e75797ac2d28c4cd5a6b2a
Carlos Lopez <calo_lrc@hotmail.com>
2019-04-18 17:01:34 -0500

    All styles and responsive mediaqueries ready!

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,22 @@ const htmlWebpack = new HtmlWebpackPlugin({
   template: './assets/index.template.html',
   filename: 'index.html'
 });
+
+const htmlWebpackDesigns = new HtmlWebpackPlugin({
+  template: './assets/designs.template.html',
+  filename: 'designs.html'
+});
+
+const htmlWebpackAbout = new HtmlWebpackPlugin({
+  template: './assets/about.template.html',
+  filename: 'about.html'
+});
+
+const htmlWebpackApps = new HtmlWebpackPlugin({
+  template: './assets/apps.template.html',
+  filename: 'apps.html'
+});
+
 module.exports = {
   entry: './assets/javascript/entry.js',
   output: {
@@ -13,7 +29,7 @@ module.exports = {
     filename: 'dist/javascript/bundle.js'
   },
   mode: 'development',
-  plugins: [htmlWebpack],
+  plugins: [htmlWebpack, htmlWebpackDesigns, htmlWebpackAbout, htmlWebpackApps],
   module: {
     rules: [
       {
