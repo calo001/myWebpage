a8e58228c8e84f352f02c1f328d85b06977d3d75
Carlos Lopez <calo_lrc@hotmail.com>
2019-04-21 10:50:31 -0500

    Compress image to improve performance

--- assets/javascript/entry.js
+++ assets/javascript/entry.js
@@ -11,3 +11,21 @@ elementsMenu.forEach( (el)=> {
     menuActive.classList.remove("menu_active");
   };
 } );
+
+// Lazy load
+
+let lazyImages = [...document.querySelectorAll('.lazy')]
+let inAdvance = 300;
+
+function lazyLoad() {
+  lazyImages.forEach(image => {
+    if (image.offsetTop < window.innerHeight + window.pageYOffset + inAdvance) {
+      image.src = image.dataset.src;
+      image.onload = () => image.classList.remove('lazy')
+    }
+  });
+}
+
+lazyLoad();
+window.addEventListener('scroll', lazyLoad, 50);
+window.addEventListener('resize', lazyLoad, 50);
